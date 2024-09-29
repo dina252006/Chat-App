c9bc409bc03c5fc97aaee26127d411ba6b01ad57
dina252006 <161189192+dina252006@users.noreply.github.com>
2024-09-29 23:36:12 +0500

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,19 @@
+const sqlite3= require("sqlite3").verbose();
+
+
+
+const db = new sqlite3.Database("chat.db");
+
+
+
+db.serialize(() => {
+    db.all("SELECT * FROM messages", (err, rows) => {
+        if(err){
+            console.error(err.message);
+            return;
+        }
+        rows.forEach((row) => {
+            console.log(`${row.timestamp} - ${row.username}: ${row.message}`);
+        });
+    });
+});
